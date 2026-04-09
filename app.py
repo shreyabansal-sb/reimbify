3ca11d6f1f988f1cb664b89d53aaef56ba23ca6f
Shreya Bansal <253486207+shreyabansal-sb@users.noreply.github.com>
2026-04-09 18:29:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -584,7 +584,7 @@ def admin_dashboard():
     )
 
 
-@app.route('/admin/requests')
+@app.route('/admin/request')
 def all_requests():
     """
     Full history — approved, rejected, pending, everything.
@@ -594,7 +594,7 @@ def all_requests():
         return redirect(url_for('login'))
 
     return render_template(
-        'admin/requests.html',
+        'admin/request.html',
         user_name = session['user_name'],
         requests  = get_all_requests()
     )
